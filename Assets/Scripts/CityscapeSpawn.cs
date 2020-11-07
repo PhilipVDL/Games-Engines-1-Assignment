3316f71ff7d2a97d443e28d2b4fdb2eb08527e4d
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2020-11-07 21:26:25 +0000

    Grid Spawning

--- Assets/Scripts/CityscapeSpawn.cs
+++ Assets/Scripts/CityscapeSpawn.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.XPath;
 using UnityEngine;
 
 public class CityscapeSpawn : MonoBehaviour
@@ -8,6 +9,11 @@ public class CityscapeSpawn : MonoBehaviour
     GameObject[] floors;
     public int falseFloors;
     public bool spawned;
+
+    //variables
+    float gridDistance = 50;
+    float falseFloorHeight = 6.5f;
+    int gridRange = 1;
 
     private void Awake()
     {
@@ -19,29 +25,67 @@ public class CityscapeSpawn : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player") && !spawned)
         {
-            //spawn street
-            Vector3 pos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 50);
-            GameObject newStreet = Instantiate(street, pos, transform.rotation);
-            //spawn ground floor
-            Vector3 streetPos = new Vector3(newStreet.transform.position.x, newStreet.transform.position.y, newStreet.transform.position.z);
-            GameObject groundFloor = Instantiate(gFloor, streetPos, newStreet.transform.rotation);
-            groundFloor.transform.SetParent(newStreet.transform);
-            //spawn false floors
-            for(int i = 0; i < falseFloors -1; i++)
+            CleanSlate();
+            SpawnCityscape();
+            spawned = true;
+        }
+    }
+
+    void CleanSlate()
+    {
+        gameObject.tag = "Untagged";
+        GameObject[] streets = GameObject.FindGameObjectsWithTag("Street");
+        foreach(GameObject go in streets)
+        {
+            Destroy(go);
+        }
+        gameObject.tag = "Street";
+    }
+
+    void SpawnCityscape()
+    {
+        for (int xPos = -gridRange; xPos <= gridRange; xPos++)
+        {
+            for (int zPos = -gridRange; zPos <= gridRange; zPos++)
             {
-                Vector3 nextLevel = new Vector3(streetPos.x, streetPos.y + (6.5f * (i + 1)) + (6.5f/2), streetPos.z);
-                GameObject nextFloor = Instantiate(fFloor, nextLevel, newStreet.transform.rotation);
-                floors[i] = nextFloor;
-                if(i == 0)
+                if (xPos == 0 && zPos == 0)
                 {
-                    nextFloor.transform.SetParent(newStreet.transform);
+                    //do nothing
                 }
                 else
                 {
-                    nextFloor.transform.SetParent(floors[i - 1].transform);
+                    SpawnStreet(xPos, zPos);
                 }
             }
-            spawned = true;
+        }
+    }
+
+    void SpawnStreet(int x, int z)
+    {
+        //street
+        Vector3 pos = new Vector3(transform.position.x + (x * gridDistance), transform.position.y, transform.position.z + (z * gridDistance));
+        GameObject newStreet = Instantiate(street, pos, transform.rotation);
+        newStreet.name = "street";
+        //ground floor
+        Vector3 streetPos = new Vector3(newStreet.transform.position.x, newStreet.transform.position.y + 0.01f, newStreet.transform.position.z);
+        GameObject groundFloor = Instantiate(gFloor, streetPos, newStreet.transform.rotation);
+        groundFloor.name = "ground floor";
+        groundFloor.transform.SetParent(newStreet.transform);
+        //false floors
+        for (int i = 0; i < falseFloors - 1; i++)
+        {
+            Vector3 nextLevel = new Vector3(streetPos.x, streetPos.y + (falseFloorHeight * (i + 1)) + (falseFloorHeight / 2), streetPos.z);
+            GameObject nextFloor = Instantiate(fFloor, nextLevel, newStreet.transform.rotation);
+            nextFloor.name = "false floor";
+            floors[i] = nextFloor;
+            if (i == 0)
+            {
+                nextFloor.transform.SetParent(newStreet.transform);
+            }
+            else
+            {
+                nextFloor.transform.SetParent(floors[i - 1].transform);
+            }
         }
     }
 }
