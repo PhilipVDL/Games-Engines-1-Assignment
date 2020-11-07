66705fd6dd363b54a5065b027b6e19a35b64c35d
PhilipVDL <61231557+PhilipVDL@users.noreply.github.com>
2020-11-07 20:06:23 +0000

    Basic Recursive Spawning

---
+++ Assets/Scripts/CityscapeSpawn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityscapeSpawn : MonoBehaviour
+{
+    public GameObject street, gFloor, fFloor;
+    GameObject[] floors;
+    public int falseFloors;
+    public bool spawned;
+
+    private void Awake()
+    {
+        spawned = false;
+        floors = new GameObject[falseFloors];
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !spawned)
+        {
+            //spawn street
+            Vector3 pos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 50);
+            GameObject newStreet = Instantiate(street, pos, transform.rotation);
+            //spawn ground floor
+            Vector3 streetPos = new Vector3(newStreet.transform.position.x, newStreet.transform.position.y, newStreet.transform.position.z);
+            GameObject groundFloor = Instantiate(gFloor, streetPos, newStreet.transform.rotation);
+            groundFloor.transform.SetParent(newStreet.transform);
+            //spawn false floors
+            for(int i = 0; i < falseFloors -1; i++)
+            {
+                Vector3 nextLevel = new Vector3(streetPos.x, streetPos.y + (6.5f * (i + 1)) + (6.5f/2), streetPos.z);
+                GameObject nextFloor = Instantiate(fFloor, nextLevel, newStreet.transform.rotation);
+                floors[i] = nextFloor;
+                if(i == 0)
+                {
+                    nextFloor.transform.SetParent(newStreet.transform);
+                }
+                else
+                {
+                    nextFloor.transform.SetParent(floors[i - 1].transform);
+                }
+            }
+            spawned = true;
+        }
+    }
+}
---
+++ Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player collision");
+        }
+    }
+}
